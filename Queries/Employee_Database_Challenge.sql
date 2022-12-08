f881bbaec68e1c5168caf07d1d02453c9a7e2033
Brenton-Fender <105960365+Brenton-Fender@users.noreply.github.com>
2022-12-07 21:20:55 -0500

    Update SQL file with queries.

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -29,3 +29,41 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count(title) DESC;
+
+--deliverable 2
+--MENTORSHIP ELIGIBILITY
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t on(e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01'
+							AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no
+
+--mentor by department
+select dept_name, count(dept_name)
+from mentorship_eligibility
+join dept_emp on
+mentorship_eligibility.emp_no = dept_emp.emp_no
+join departments on 
+dept_emp.dept_no = departments.dept_no
+where dept_emp.to_date = '9999-01-01'
+group by dept_name;
+
+--mentor by title
+select title, count(title)
+from mentorship_eligibility
+join dept_emp on
+mentorship_eligibility.emp_no = dept_emp.emp_no
+join departments on 
+dept_emp.dept_no = departments.dept_no
+where dept_emp.to_date = '9999-01-01'
+group by title 
+
+
